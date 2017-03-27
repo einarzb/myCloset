107b7d1bfe124a437a6bf79d3410b3c0bade0f6d
einarzb <einarzb@gmail.com>
2017-03-27 21:25:34 +0300

    added awesome gif and color picker to my form

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,34 @@
 var app = angular.module('closetApp', ['ui.router','color.picker']);
 
-app.config(['$stateProvider','$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-  //hashbang fix
-  $locationProvider.html5Mode(true);
-
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      controller:'mainController',
-      templateUrl: '/templates/home.html'
-    })
-    .state('beer', {
-      url: '/reviews/:id',
-      params: {
-      	beerParam: null
-      },
-      controller: 'beerController',
-      templateUrl: '/templates/beer.html'
-    })
-    .state('register', {
-      url: '/register',
-      templateUrl: '/templates/register.html',
-      controller: 'authController'
-    })
-    .state('login', {
-      url: '/login',
-      templateUrl: '/templates/login.html',
-      controller: 'authController'
-    })
-
-    //'default' state
-    $urlRouterProvider.otherwise('/home');
-}]);
+// app.config(['$stateProvider','$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+//   //hashbang fix
+//   $locationProvider.html5Mode(true);
+//
+  // $stateProvider
+  //   .state('home', {
+  //     url: '/home',
+  //     controller:'mainController',
+  //     templateUrl: '/templates/home.html'
+  //   })
+  //   .state('beer', {
+  //     url: '/reviews/:id',
+  //     params: {
+  //     	beerParam: null
+  //     },
+  //     controller: 'beerController',
+  //     templateUrl: '/templates/beer.html'
+  //   })
+  //   .state('register', {
+  //     url: '/register',
+  //     templateUrl: '/templates/register.html',
+  //     controller: 'authController'
+  //   })
+  //   .state('login', {
+  //     url: '/login',
+  //     templateUrl: '/templates/login.html',
+  //     controller: 'authController'
+  //   })
+  //
+  //   //'default' state
+  //   $urlRouterProvider.otherwise('/home');
+// }]);
