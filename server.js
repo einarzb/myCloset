0f20417c76dfb3ab6fd2f89b058667a6a6792826
einarzb <einarzb@gmail.com>
2017-05-24 12:05:18 +0300

    uncommented social register

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/closetdb', function(req, res, next) {
 });
 
 //adding looks
-app.post('/looks', function(req, res, next) {
+app.post('/looks', ensureAuthenticated, function(req, res, next) {
   Look.create(req.body, function(err, item) {
     if (err) {
       console.error(err)
